3ee5088a4c7d7febb825dc48a283f3e6ab028773
ATRVIIE <119594378+ATRVIIE@users.noreply.github.com>
2023-05-06 13:35:24 +0200

    Add files via upload

---
+++ atr_pack_free/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+author 'ATR#4444' 
+
+files {
+  'stream/atr_pack_free_set_props.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/atr_pack_free_set_props.ytyp'
